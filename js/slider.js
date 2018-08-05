8ea0206252e9949bda6f63bb3a62981d6140f482
nurik99 <40524631+nurik99@users.noreply.github.com>
2018-08-05 19:06:11 +0600

    Added Slider

---
+++ js/slider.js
@@ -0,0 +1,99 @@
+var slider = document.getElementById('slider');
+var slider_list = document.getElementsByClassName('slider-item');
+var len = slider_list.length;
+var current_slide = 0;
+var indicator_list = document.getElementsByClassName('slider-ind__slide');
+
+slider.style.width = (len * 100)+ "%";
+
+for (var i = 0; i <len; i++){
+	slider_list[i].style.width = (100/len) + "%";
+}
+
+function setSlide() {
+	slider.style.left = (-100 * current_slide) + '%';
+}
+
+function setActive() {
+	indicator_list[current_slide].className = 'slider-ind__slide active';
+}
+
+function removeActive() {
+	indicator_list[current_slide].className = 'slider-ind__slide';
+}
+
+function nextSlide() {
+	clearInterval(timerId);
+	if (current_slide == len - 1) {
+		removeActive();
+		current_slide = len - len;
+		setActive();
+		setSlide();
+	}
+	else {
+		removeActive();
+		current_slide++;
+		setActive();
+		setSlide();
+	}
+	setTimeout(function() {
+  		slider_nurik();
+	}, 10000);
+}
+
+function backSlide(){
+	clearInterval(timerId);
+	if (current_slide == 0){
+		removeActive();
+		current_slide = len - 1;
+		setActive();
+		setSlide();
+	}
+	else {
+		removeActive();
+		current_slide--;
+		setActive();
+		setSlide();
+	}
+	setTimeout(function() {
+  		slider_nurik();
+	}, 10000);
+}
+
+function setSlidebyIndicator(index) {
+	removeActive();
+	current_slide = index;
+	setActive();
+	setSlide();
+}
+var time = 6000;
+var timerId = setInterval(function() {
+	if (current_slide == len - 1) {
+		removeActive();
+		current_slide = len - len;
+		setActive();
+		setSlide();
+	}
+	else {
+		removeActive();
+		current_slide++;
+		setActive();
+		setSlide();
+	}
+}, time);
+slider_nurik = () => {
+	var timerId = setInterval(function() {
+		if (current_slide == len - 1) {
+			removeActive();
+			current_slide = len - len;
+			setActive();
+			setSlide();
+		}
+		else {
+			removeActive();
+			current_slide++;
+			setActive();
+			setSlide();
+		}
+	}, time);
+}
