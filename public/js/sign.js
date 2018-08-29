2d4aefd68906c16cda81cf79c3fab2c0fe500aeb
nurik99 <40524631+nurik99@users.noreply.github.com>
2018-08-29 21:40:42 +0600

    added sign-in style, other...

--- public/js/sign.js
+++ public/js/sign.js
@@ -1,12 +1,18 @@
-function signIn() {
-	var signIn = document.getElementById('sign-in');
-	var backdropSign = document.getElementById('backdrop');
-	var signInDiv = document.getElementById('sign-in__div')
-	if(backdropSign.style.display == "none") {
-		signIn.style.display = "flex";
-		backdropSign.style.display = "block";
-	}else {
-		signIn.style.display = "none";
-		backdropSign.style.display = "none";
-	}
+var signIn = document.getElementById('sign-in');
+var headerAuthSign = document.getElementById('header-auth-sign');
+headerAuthSign.onmouseover = function () {
+	signIn.style.cssText = "display: block;";
+	headerAuthSign.style.cssText = "background-color: #F6A263;";
+}
+headerAuthSign.onmouseout = function () {
+	signIn.style.display = "none";
+	headerAuthSign.style.backgroundColor = "#fff";
+}
+signIn.onmouseover = function () {
+	signIn.style.display = "block";
+	headerAuthSign.style.backgroundColor = "#F6A263";
+}
+signIn.onmouseout = function () {
+	signIn.style.display = "none";
+	headerAuthSign.style.backgroundColor = "#fff";
 }
